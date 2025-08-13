78fa3630dd9e8d48ce1a2e4eda681104940389de
Hüseyin Tüğyan <hsyntugyan@gmail.com>
2025-08-13 18:28:50 +0300

    upd doing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex :1}}>
+        <Stack>
+          <Stack.Screen name="index" options={{ title:"Feed"}} />
+          <Stack.Screen 
+            name="notifiations" 
+            options={{ title:"Notifications" , 
+            headerShown: false}}
+          />
+        </Stack>
+    </SafeAreaView>
+    </SafeAreaProvider>
+  
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { View } from "react-native";
+import { styles } from "../styles/auth.styles";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.title}>Hello</Text>
+    <View style={styles.container}>
+      <Link href={"/notifications"}>visit notifications screen</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
-  },
-  title: {
-    color:"red",
-    fontSize: 80,
-  },
-})

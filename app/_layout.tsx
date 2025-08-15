d6080f096f33d368aa0f9f456f55a3ccf8cbb65f
Hüseyin Tüğyan <hsyntugyan@gmail.com>
2025-08-15 18:08:14 +0300

    Authentication doing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,26 @@
-import { Stack } from "expo-router";
+import { tokenCache } from "@/cache";
+import InitialLayout from "@/components/InitialLayout";
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+const publishabKey= process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if(!publishabKey){
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  )
+}
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex :1, backgroundColor:"#000"}}>
-        <Stack screenOptions={{ headerShown: false }} />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishabKey}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex :1, backgroundColor:"#000"}}>
+            <InitialLayout />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   
   );
 }
